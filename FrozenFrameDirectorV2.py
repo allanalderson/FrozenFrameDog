8c2aabf51bcb98a5315e9ad60e48dc0474d053a3
Allan Alderson <adsaalderson@gmail.com>
2024-04-17 16:01:27 +1000

    update

--- FrozenFrameDirectorV2.py
+++ FrozenFrameDirectorV2.py
@@ -3,14 +3,15 @@ import pyautogui
 import threading
 import time
 
-print("FROZEN FRAME DIRECTOR\n")
+print("FROZEN FRAME DIRECTOR v2\n")
 print("NOTE:   ENSURE NDI WEBCAM 1 IS IN SET TO 480p30 BEFORE RUNNING THIS CODE.\n")
+print("Starting in 5 seconds\n")
 ####################
-overlay1_timer = 60 # seconds
+overlay2_timer = 60 # seconds
 frame_distance = 30  # (30 = 1sec at 30fps) Higher values are more forgiving of occasional freezes
 show_diff_window = False  # True or False
 difference_threshold = 5000  # Default 5000   Keep value below 20000 (480p)
-retrigger_holdoff = 5  # seconds.
+retrigger_holdoff = 2  # seconds.
 cap = cv2.VideoCapture(0)  # 0 is NDI webcam 1
 ####################
 start_time = time.time()
@@ -19,18 +20,15 @@ frame_loss = False
 ret, frame1 = cap.read()
 time.sleep(1)
 ret, frame2 = cap.read()
-time.sleep(3)
+time.sleep(4)
 no_overlays_rule = 0
 overlay1_in_time = 0
-print("Running!")
-def overlay_function():
-	if frame_loss == False: # Don't overlay during the adverts!
-		print(" Overlay 3 In - F3 ")
-		pyautogui.press('f3')     # press a function key
-def rules_function():
-	if frame_loss == True:
-		print(" All Overlays OFF  - F12 ")
-		pyautogui.press('f12')     # press a function key
+print("Running \n")
+def overlay2_timer_function():
+	if frame_loss == False: # Only if there's no frame loss occurring
+		print(" Overlay 2 In - F2 ")
+		pyautogui.press('f2')     # press a function key
+
 
 def reset_frame_loss():
 	global frame_loss
@@ -57,17 +55,14 @@ while True:
 			cv2.imshow(' Difference       (use q to exit)', enhanced_diff)
 		if pixel_difference_count < difference_threshold and frame_loss == False:
 			frame_loss = True
-			print(" Frame Loss \n")
-			pyautogui.press('f2')     # press a function key
+			# print(" Frame Loss  F4\n")
+			pyautogui.press('f4')     # press a function key
 			threading.Thread(target=reset_frame_loss).start()
 	current_time = time.time()
-	if current_time - overlay1_in_time >= overlay1_timer:  # Seconds
-		overlay_function()
+	if current_time - overlay1_in_time >= overlay2_timer:  # Seconds
+		overlay2_timer_function()
 		overlay1_in_time = current_time
 
-	if current_time - no_overlays_rule >= .5:  # Seconds
-		rules_function()
-		no_overlays_rule = current_time
 
 cap.release()
 cv2.destroyAllWindows()
